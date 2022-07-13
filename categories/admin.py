4d95e5f70ccf872a3bca754b7faa56f78daa86dd
mahmoudabuelnaga <mahmoudaboelnaga392@gmail.com>
2022-07-13 02:32:43 +0200

    update admin

--- categories/admin.py
+++ categories/admin.py
@@ -4,14 +4,21 @@ from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 @admin.register(Category)
-class RecitationsAdmin(ImportExportModelAdmin):
-    pass
+class CategoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['title_ar', 'title', 'number']
+    search_fields = ['title_ar', 'title','number']
+    list_editable = ['number']
 
 @admin.register(Container)
-class RecitationsAdmin(ImportExportModelAdmin):
-    pass
-
+class ContainerAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['title_ar', 'title', 'number', 'category']
+    list_filter = ['category']
+    search_fields = ['title_ar', 'title','number']
+    list_editable = ['number']
 
 @admin.register(Component)
-class RecitationsAdmin(ImportExportModelAdmin):
-    pass
+class ComponentAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['container', 'number']
+    list_filter = ['container']
+    search_fields = ['body', 'surat','bismillah']
+    list_editable = ['number',]
