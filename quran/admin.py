52665e07e878eb34fabe371f7255efd139d2604e
mahmoudabuelnaga <mahmoudaboelnaga392@gmail.com>
2022-07-13 17:29:45 +0200

    update quran admin

--- quran/admin.py
+++ quran/admin.py
@@ -4,15 +4,15 @@ from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 @admin.register(Reader)
-class ReaderAdmin(ImportExportModelAdminm, admin.ModelAdmin):
-    list_display = ['name_ar', 'name']
-    search_fields = ['name_ar', 'name']
-    list_editable = ['name_ar', 'name']
+class ReaderAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['name', 'name_ar',]
+    search_fields = ['name', 'name_ar',]
+    # list_editable = ['name', 'name_ar',]
 
 
 @admin.register(Recitations)
 class RecitationsAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    list_display = ['reader', 'number', 'name_ar', 'name','surat_time']
+    list_display = ['name', 'reader', 'number', 'name_ar', 'surat_time']
     list_filter = ['reader']
     search_fields = ['reader', 'number', 'name_ar']
     list_editable = ['number', 'name_ar', 'surat_time']
